5782bb999085b38611a42edef4095d8d52624df0
David Hughes <44324880+hughesd9@users.noreply.github.com>
2020-08-02 21:21:46 +0100

    Add files via upload

---
+++ src/TodoItem/todoItem.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TodoItem extends React.Component {
+  render() {
+    return(<div> Hello from the todo item component </div>);
+  }
+}
+
+export default TodoItem;
