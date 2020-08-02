b05a1e2d3d3089bfc7582d64f43b6e4f5392e4f0
David Hughes <44324880+hughesd9@users.noreply.github.com>
2020-08-02 21:20:54 +0100

    Add files via upload

---
+++ src/TodoList/todoList.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class TodoList extends React.Component {
+  render() {
+    return(<div> Hello from the todo list component </div>);
+  }
+}
+
+export default TodoList;
