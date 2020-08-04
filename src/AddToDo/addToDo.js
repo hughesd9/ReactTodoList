8f0e04e29183599b5074ba0b84f90c365e9cf7d8
David Hughes <44324880+hughesd9@users.noreply.github.com>
2020-08-04 18:25:21 +0100

    Update addToDo.js

--- src/AddToDo/addToDo.js
+++ src/AddToDo/addToDo.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
 class AddToDo extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      todo: ''
+    };
+  }
+
   render() {
-    return(<div> Hello from the add todo component </div>);
+    return(<div className='addTodoContainer'>
+      <form onSubmit={(e) => this.submitTodo(e)}>
+        <input id='addTodoInput' onChange={(e) => this.updateInput(e)} type='text'></input>
+        <button type='submit'> Add Todo </button>
+      </form>
+    </div>);
+  }
+
+  updateInput = (e) => {
+    this.setState({todo: e.target.value})
+  }
+
+  submitTodo = (e) =>{
+    e.preventDefault();
+    this.props.addTodoFunction(this.state.todo);
+    document.getElementById('addTodoInput').value = '';
   }
 }
+
 
 export default AddToDo;
